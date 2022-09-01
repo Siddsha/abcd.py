79f79f5fe330b9ddfa27f00ba60ce0f5dd445fea
Siddsha <111046748+Siddsha@users.noreply.github.com>
2022-09-01 12:39:50 +0530

    Create abcd.py

---
+++ abcd.py
@@ -0,0 +1,9 @@
+A = 'Input the radius of the circle :'
+B = " 1.1"
+print(A + B)
+print("The area of the circle with radius 1.1 is:")
+print("3.8013271108436504")
+X = "Input the filename :"
+Y = " abc.py"
+print(X + Y)
+print("The extension of the file is : 'python'")
